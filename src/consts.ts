1ec9d7847921c20a31b4d9feb097bd1004dbe129
Robbie Prokop <robbieprokop@gmail.com>
2023-06-16 14:08:26 -0600

    add title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Perma-Cast Concrete Construction LTD";
+export const SITE_DESCRIPTION =
+  "Perma-Cast Concrete Contruction continues to raise the bar in all disciplines, including commercial and industrial concrete foundations, multi-level parkades, high-rises, and water reservoirs.";
