4f3c83431c0423016dc00c87f7b77b6b0bc39e7b
Robbie Prokop <robbieprokop@gmail.com>
2023-06-22 10:01:02 -0600

    working on management and styles

---
+++ src/content/team/management.js
@@ -0,0 +1,26 @@
+export const management = [
+  {
+    name: "JOHN MCLEOD",
+    position: "Executive Board Member",
+  },
+  {
+    name: "TYLER PREER",
+    position: "President",
+  },
+  {
+    name: "SHELLY MCLEOD",
+    position: "Chief Financial Officer",
+  },
+  {
+    name: "JAMIE WOOD",
+    position: "Chief Director of Operations",
+  },
+  {
+    name: "SEAN CRIBBETT",
+    position: "Chief Construction Manager",
+  },
+  {
+    name: "KENDRA HICKEY",
+    position: "Chief Information/Innovation/Compliance Officer",
+  },
+];
