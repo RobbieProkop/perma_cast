5ce59675e633b790f2af9ba4ec915c08b75cb5d2
Robbie Prokop <robbieprokop@gmail.com>
2023-06-22 12:45:08 -0600

    adding images to management

--- src/content/team/management.js
+++ src/content/team/management.js
@@ -2,25 +2,31 @@ export const management = [
   {
     name: "JOHN MCLEOD",
     position: "Executive Board Member",
+    image: "/assets/team/management/shelly.jpeg",
   },
   {
     name: "TYLER PREER",
     position: "President",
+    image: "/assets/team/management/tyler.jpg",
   },
   {
     name: "SHELLY MCLEOD",
     position: "Chief Financial Officer",
+    image: "/assets/team/management/shelly.jpeg",
   },
   {
     name: "JAMIE WOOD",
     position: "Chief Director of Operations",
+    image: "/assets/team/management/jamie.jpeg",
   },
   {
     name: "SEAN CRIBBETT",
     position: "Chief Construction Manager",
+    image: "/assets/team/management/shelly.jpeg",
   },
   {
     name: "KENDRA HICKEY",
     position: "Chief Information/Innovation/Compliance Officer",
+    image: "/assets/team/management/kendra.jpg",
   },
 ];
